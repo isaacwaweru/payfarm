6d2f71c5d79eaccc0d1253f50d82871d04387795
christopherndugo <christopherndugo@qhala.com>
2022-02-09 12:50:33 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="#fff"
+      dark
+      flat
+    >
+      <div class="d-flex align-center">
+        <v-img
+            lazy-src="./assets/foot.svg"
+             max-height="140"
+             max-width="140"
+            src="./assets/foot.svg"
+          ></v-img>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        color="#53C351"
+        height="35"
+      >
+      Login
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,400 @@
+<template>
+<div>
+  <div class="hero-1">
+   <v-row justify="center">
+     <v-col cols="12" sm="4">
+       <h1 class="heading-1">Empowering Farming</h1>
+       <h1 class="heading-2">through Technology</h1>
+     </v-col>
+   </v-row>
+
+   <v-row justify="center">
+     <v-col cols="12" sm="4">
+       <p style="color:white;">We help realize your dreams of investing in Agriculture,<br></p>
+       <p style="color:white;">let's start with small things that can change the world,<br></p>
+       <p style="color:white;" class="ml-16">so we can live happy and abundant lives.<br></p>
+     </v-col>
+   </v-row>
+
+   <v-row justify="center">
+     <v-col cols="12" sm="4">
+       <p style="color:white;">We help realize your dreams of investing in Agriculture<br></p>
+       <p style="color:white;" class="ml-16">sell it on the site and earn profits<br></p>
+     </v-col>
+   </v-row>
+  </div>
+
+<div class="packages">
+  <v-container>
+    <v-row justify="center" align="center">
+     <v-col cols="12" sm="2">
+   <h2 class="heading-3  mt-6">How it works</h2>
+    </v-col>
+      </v-row>
+
+    <v-row justify="center">
+     <v-col cols="12" sm="3">
+   <h3 class="heading-4">Sell your animal once its grown</h3>
+    </v-col>
+      </v-row>
+  </v-container>
+
+   <v-row justify="space-around" class="section-2">
+     <v-col
+      cols="12"
+      sm="3"
+     >
+       <v-img
+            lazy-src="../assets/register.svg"
+             max-height="80"
+             max-width="60"
+            src="../assets/register.svg"
+            class="ml-16"
+          ></v-img>
+       <h3 class="sub-head ml-16">Register</h3>
+       <p>Register using your details and get <br>verified</p>
+     </v-col>
+
+     <v-col
+     cols="12"
+      sm="3"
+     >
+       <v-img
+            lazy-src="../assets/buy.svg"
+             max-height="80"
+             max-width="60"
+            src="../assets/buy.svg"
+            class="ml-16"
+          ></v-img>
+       <h3 class="sub-head ml-6">Buy a young one</h3>
+       <p>Select your package/ animal 
+    <br>investment in a select time frame</p>
+     </v-col>
+
+     <v-col
+      cols="12"
+      sm="3"
+     >
+       <v-img
+            lazy-src="../assets/feed.svg"
+             max-height="80"
+             max-width="60"
+            src="../assets/feed.svg"
+          ></v-img>
+       <h3 class="sub-head">Feed your animal</h3>
+       <p>Feed your animal and check on<br> progress</p>
+     </v-col>
+
+     <v-col
+      cols="12"
+      sm="3"
+     >
+       <v-img
+            lazy-src="../assets/sell.svg"
+             max-height="80"
+             max-width="60"
+            src="../assets/sell.svg"
+          ></v-img>
+       <h3 class="sub-head">Sell your chicken</h3>
+       <p>Cash out once your<br> chicken has blossom</p>
+     </v-col>
+   </v-row>
+
+    <v-row justify="space-around">
+    <v-col 
+    cols="12"
+    sm="3" >
+      <div class="card-1 mt-16 mb-5">
+        <div class="card-2">
+          <h1 style="color:#231D4F;font-weight:500;padding-top:25px!important;" class="mt-6 ml-6">k.500 <span style="color:#848199;font-weight:300;font-size:18px;">/month</span></h1>
+          <h2 style="color:#231D4F;font-weight:500;" class="ml-6">Chick</h2>
+          <v-divider width="250" class="ml-6"/>
+          <p  style="color:#848199;font-weight:5300;padding-top:5px!important;" class="ml-6 mt-4">Pay and grow your fam animals</p>
+          <v-img
+            lazy-src="../assets/sell.svg"
+             max-height="120"
+             max-width="120"
+            src="../assets/chick.svg"  
+            style="margin:auto!important;"
+            class="mt-16"
+          ></v-img>
+        </div>
+
+        <div class="action">
+          PROCEED TO BUY
+        </div>
+      </div>
+    </v-col>
+
+    <v-col 
+    cols="12"
+    sm="3" >
+      <div class="card-1-r mt-16 mb-5">
+        <div class="card-2">
+          <h1 style="color:#231D4F;font-weight:500;padding-top:25px!important;" class="mt-6 ml-6">k.500 <span style="color:#848199;font-weight:300;font-size:18px;">/month</span></h1>
+          <h2 style="color:#231D4F;font-weight:500;" class="ml-6">Calf</h2>
+          <v-divider width="250" class="ml-6"/>
+          <p style="color:#848199;font-weight:5300;padding-top:5px!important;" class="ml-6 mt-4">Pay and grow your fam animals</p>
+          <v-img
+            lazy-src="../assets/sell.svg"
+             max-height="130"
+             max-width="120"
+            src="../assets/calf.svg"
+            style="margin:auto!important;"
+            class="mt-16"
+          ></v-img>
+        </div>
+
+        <div class="action">
+          COMING SOON
+        </div>
+      </div>
+    </v-col>
+
+    <v-col 
+    cols="12"
+    sm="3" >
+      <div class="card-1-r-r mt-16 mb-5">
+        <div class="card-2">
+          <h1 style="color:#231D4F;font-weight:500;padding-top:25px!important;" class="mt-6 ml-6">k.500 <span style="color:#848199;font-weight:300;font-size:18px;">/month</span></h1>
+          <h2 style="color:#231D4F;font-weight:500;" class="ml-6">Kid</h2>
+          <v-divider width="250" class="ml-6"/>
+          <p style="color:#848199;font-weight:5300;padding-top:5px!important;" class="ml-6 mt-4">Pay and grow your fam animals</p>
+          <v-img
+            lazy-src="../assets/sell.svg"
+             max-height="120"
+             max-width="120"
+            src="../assets/kid.svg"
+            style="margin:auto!important;"
+            class="mt-16"
+          ></v-img>
+        </div>
+
+        <div class="action">
+          COMING SOON
+        </div>
+      </div>
+    </v-col>
+
+  </v-row>
+  </div>
+
+<div class="footer-1">
+  <div class="form">
+    <v-img
+            lazy-src="../assets/foot.svg"
+            src="../assets/head.svg"
+          ></v-img>
+          <div class="form-1">
+            <v-img
+            lazy-src="../assets/foot.svg"
+            src="../assets/avr.svg"
+          ></v-img>
+          </div>
+    <form>
+      <h3>Phone number</h3>
+    <v-text-field
+      v-model="name"
+      label="Phone number"
+      required
+      outlined
+    ></v-text-field>
+     <h3>Password</h3>
+    <v-text-field
+      v-model="name"
+      label="Password"
+      required
+      outlined
+    ></v-text-field>
+    <v-checkbox
+      v-model="checkbox"
+      label="Remember me?"
+      required
+      max-width="200"
+    ></v-checkbox>
+
+    <v-btn
+      color="#556EE6"
+      dark
+      width="350"
+    >
+      Login
+    </v-btn>
+    <h5><v-icon>mdi-lock</v-icon> Forgot your password?</h5>
+    <h4>Don't have an account?<span style="color:#E1A11C;">Signup now</span></h4>
+    </form>
+  </div>
+</div>
+
+<div class="footer-2">
+  <v-row  justify="center">
+    <v-col
+    cols="12"
+    sm="2"
+    >
+      <v-img
+            lazy-src="../assets/foot.svg"
+             max-height="150"
+             max-width="120"
+            src="../assets/foot.svg"
+            class="mt-9"
+          ></v-img>
+    </v-col>
+  </v-row>
+
+  <v-row  justify="center">
+    <v-col
+    cols="12"
+    sm="3"
+    >
+    <h5 style="color:#fff;font-size:14px; opacity: 0.8; font-weight:300;" class="ml-5">The best way to make invetments</h5>
+    </v-col>
+  </v-row>
+
+  <v-row  justify="center">
+    <v-col
+    cols="12"
+    sm="2"
+    >
+    <div class="box">
+          <h5 style="color:#fff;font-size:14px; opacity: 0.8; font-weight:600;">Contact Us</h5>
+          <h5 style="color:#fff;font-size:14px; opacity: 0.8; font-weight:600;" class="ml-5">FAQs</h5>
+        </div>
+    </v-col>
+  </v-row>
+
+  <v-container>
+    <v-row  justify="center">
+    <v-col
+    cols="12"
+    sm="2"
+    >
+    <div class="box ml-4">
+      <v-icon>mdi-facebook</v-icon>
+      <v-icon>mdi-twitter</v-icon>
+      <v-icon>mdi-instagram</v-icon>
+        </div>
+    </v-col>
+  </v-row>
+  </v-container>
+</div>
+</div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      home: "Here there is the home page!"
+    }),
+  }
+</script>
+
+<style scoped>
+.box {
+      display: flex;
+      gap: 30px;
+    }
+.v-text-field{
+      width: 350px;
+}
+.v-icon{
+  color: #fff;
+}
+.hero-1 {
+  height: 100vh;
+  width: 100%;
+  background: url("../assets/Frame.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.heading-1 {
+  color: #fff;
+  font-size: 40px;
+  margin-top: 150px;
+  letter-spacing: 1px;
+}
+.heading-2 {
+  color: #fff;
+  font-size: 40px;
+  letter-spacing: 1px;
+}
+.heading-3{
+  color: #61BD5B;
+  text-align: center !important;
+}
+.heading-4{
+  color: black;
+  text-align: center !important;765
+}
+.section-2 {
+  margin: auto !important;
+}
+.action {
+  margin-top: -38px;
+  margin-left: 30px;
+  color: #fff;
+  font-size: 18px;
+  font-weight: 500
+}
+.sub-head{
+  color: #B16C53;
+}
+.packages{
+  background-color: #E5E5E5;
+}
+.card-1-r{
+  height: 400px;
+  width: 300px;
+  background-color: #F496D1;
+  -webkit-box-shadow: 0px -4px 8px #f0b3d9;
+  -moz-box-shadow: 0px -4px 8px #f0b3d9;
+  box-shadow: 0px -4px 8px #f0b3d9;
+}
+.card-1-r-r{
+  height: 400px;
+  width: 300px;
+  background-color: #0A0535;
+  -webkit-box-shadow: 0px -4px 8px #19134e;
+  -moz-box-shadow: 0px -4px 8px #19134e;
+  box-shadow: 0px -4px 8px #19134e;
+}
+.card-1{
+  height: 400px;
+  width: 300px;
+  background-color: #EF9956;
+  -webkit-box-shadow: 0px -4px 8px #EF9956;
+  -moz-box-shadow: 0px -4px 8px #EF9956;
+  box-shadow: 0px -4px 8px #EF9956;
+}
+.card-2{
+  height: 400px;
+  width: 290px;
+  background-color: #fff;
+  transform: translate(-20px, -50px);
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 15px 0px;
+  border-radius: 2px;
+}
+.form {
+  height: auto;
+  width: 400px;
+  background-color: #fff;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 15px 0px;
+  margin: 6px;
+  border-radius: 6px;
+}
+.form-1 {
+  transform: translate(0px, -30px);
+}
+.footer-1 {
+  height: 100vh;
+  width: 100%;
+  background: url("../assets/footer.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.footer-2 {
+  background-color: #1E2833;
+  height: auto;
+}
+</style>
+
